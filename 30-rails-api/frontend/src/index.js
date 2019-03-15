1d9ba89e4279e7342336e5cf588dde50abb742c6
Alex Griffith <alexalexgriffith@gmail.com>
2019-03-15 14:33:42 -0400

    Post lecture 30 pt ii

--- 30-rails-api/frontend/src/index.js
+++ 30-rails-api/frontend/src/index.js
@@ -4,9 +4,12 @@
   // <td>1</td>
 // </tr> -->
 
+const API_URL = 'http://localhost:3000'
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log('the DOM content has loaded');
   const tbody = document.querySelector('tbody')
+  const form = document.querySelector('form.ui.form')
 
   const createAnimalRow = (animal) => {
     // console.log('animal', animal);
@@ -14,18 +17,49 @@ document.addEventListener("DOMContentLoaded", () => {
     tr.innerHTML = `
       <td>${animal.name}</td>
       <td>${animal.diet}</td>
-      <td>${animal.species_id}</td>
+      <td>${animal.species_name}</td>
     `
 
     return tr
   }
 
-  fetch('http://localhost:3000/animals')
+  fetch(`${API_URL}/animals`)
   .then(res => res.json())
   .then(animals => {
     console.log('here is the response from the API', animals);
     animals.map(createAnimalRow).forEach(row => {
       tbody.appendChild(row)
+    })
+  })
+
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e);
+    const name = form.querySelector('#animal-name').value
+    const species_name = form.querySelector('#animal-species').value
+    const diet = form.querySelector('#animal-diet .selected').dataset.value
+
+    // const newAnimalRow = createAnimalRow({name: name})
+    // tbody.appendChild(newAnimalRow)
+    fetch(
+      `${API_URL}/animals`,
+      {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          species_name: species_name,
+          diet: diet
+        })
+      }
+    )
+    .then(res => res.json())
+    .then(data => {
+      const newAnimalRow = createAnimalRow(data)
+      tbody.appendChild(newAnimalRow)
     })
   })
 })
--- 30-rails-api/zookeepr_backend/app/controllers/animals_controller.rb
+++ 30-rails-api/zookeepr_backend/app/controllers/animals_controller.rb
@@ -1,5 +1,12 @@
 class AnimalsController < ApplicationController
   def create
+    # Animal.create({name => '', diet => 0, id => 13})
+    # Animal.create(animal_params)
+    @species = Species.find_or_create_by(name: params[:species_name])
+    #{name => '', diet => "0", id => 13}
+    @animal = Animal.create(name: params[:name], species: @species, diet: params["diet"].to_i)
+
+    render json: @animal
   end
 
   def show
@@ -12,5 +19,10 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
 
     render json: @animals
+  end
+
+  def animal_params
+    # {animal => {name => '', diet => 0}}
+    params.require(:animal).permit(:name, :diet)
   end
 end
